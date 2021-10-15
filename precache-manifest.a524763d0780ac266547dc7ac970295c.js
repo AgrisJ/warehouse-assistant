a6ca535c8c71bde7ec1fb028557c8901db1b982b
AgrisJ <agris.jurjans@gmail.com>
2021-10-15 11:55:33 +0200

    Updates

---
+++ precache-manifest.a524763d0780ac266547dc7ac970295c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c463342d2a09d48ea1770ca4e4f1925c",
+    "url": "/AgrisJ/warehouse-assistant/index.html"
+  },
+  {
+    "revision": "949029aaf5e179889494",
+    "url": "/AgrisJ/warehouse-assistant/static/css/2.803f5144.chunk.css"
+  },
+  {
+    "revision": "ab8b72a209a935c29ae3",
+    "url": "/AgrisJ/warehouse-assistant/static/css/main.fc16396d.chunk.css"
+  },
+  {
+    "revision": "949029aaf5e179889494",
+    "url": "/AgrisJ/warehouse-assistant/static/js/2.5616eda5.chunk.js"
+  },
+  {
+    "revision": "a12ae8f5711cff342dc2247d7c4394d9",
+    "url": "/AgrisJ/warehouse-assistant/static/js/2.5616eda5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab8b72a209a935c29ae3",
+    "url": "/AgrisJ/warehouse-assistant/static/js/main.6d9855c8.chunk.js"
+  },
+  {
+    "revision": "73f9c46dad84ae53d606",
+    "url": "/AgrisJ/warehouse-assistant/static/js/runtime-main.ec4f5448.js"
+  },
+  {
+    "revision": "a1e42ef3a3f7c3a2d23c9ac302a6f6f0",
+    "url": "/AgrisJ/warehouse-assistant/static/media/add.a1e42ef3.svg"
+  },
+  {
+    "revision": "c81de5732ec9ca60ad13c3322e24df83",
+    "url": "/AgrisJ/warehouse-assistant/static/media/agris.c81de573.png"
+  },
+  {
+    "revision": "fb9f5e2cb78c9f93ebecfaae2d36cf13",
+    "url": "/AgrisJ/warehouse-assistant/static/media/logo.fb9f5e2c.svg"
+  },
+  {
+    "revision": "6460756a705dab49606f244894d74d6b",
+    "url": "/AgrisJ/warehouse-assistant/static/media/subtract.6460756a.svg"
+  },
+  {
+    "revision": "de35ba6c42f0df3f35283f4627acacd6",
+    "url": "/AgrisJ/warehouse-assistant/static/media/undo.de35ba6c.svg"
+  }
+]);
